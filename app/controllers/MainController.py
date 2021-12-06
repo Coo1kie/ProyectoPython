6c7cfa4d1ef06c8ed587d8f6249fb2201f45bfdd
Coo1kie <alegm3865@gmail.com>
2021-12-05 21:32:22 -0400

    tabla Usuario succesful

--- app/controllers/MainController.py
+++ app/controllers/MainController.py
@@ -1,10 +1,12 @@
+from app.models.User import User
+from app import db
 from flask import render_template
 class MainController():
     def __init__(self):
         pass
 
     def index(self):
-        user1 = {'name': 'Tupaj','surname':'Martinez'}
-        #users = User.query.all()
-        return render_template('index.html', user=user1)
+        #users = {'name': 'Tupaj','surname':'Martinez'}
+        users = User.query.all() #obtiene todo
+        return render_template('index.html', users=users) #el primer users es el que manda al index
 maincontroller = MainController()
--- app/models/User.py
+++ app/models/User.py
@@ -4,7 +4,7 @@ from app import db
 class User(db.Model):
     __tablename__='users'
     id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    cedula=db.Column(db.String(10))
+    #cedula=db.Column(db.String(10))
     name=db.Column(db.String(50))
     email=db.Column(db.String(50))
     username=db.Column(db.String(50))
